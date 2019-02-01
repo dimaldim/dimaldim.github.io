5b2844c1b3818ca105a49f036c249cf06c0bafde
Dimitar <antraks92@gmail.com>
2019-02-01 10:13:06 +0200

    Added "Scroll to top" button, education info.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-top').fadeIn();
+        } else {
+            $('.scroll-top').fadeOut();
+        }
+    });
+
+    $('.scroll-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 100);
+        return false;
+    });
+
+});
+
 $(".nav a").click(function () {
     $("body,html").animate({
         scrollTop: $("#" + $(this).data('value')).offset().top
